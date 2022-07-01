3d09e81e8a0f1ea30d99a8effbb661d9f65432b1
Chizoba ODINAKA <chizobajames21@gmail.com>
2022-07-01 10:43:16 -0700

    I didn't sleep last night

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,17 @@
-#include <stdlib.h>
+#incluide <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /**
- * Random number generator
+ * main - Random number generator
+ * tell if a number is positive, negative or zero
+ * Return: 0
 */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n =iiiii rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
