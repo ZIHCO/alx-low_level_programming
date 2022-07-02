45a07983a995fb8e4b0fb5c8acad04cd9f856d2b
Chizoba ODINAKA <chizobajames21@gmail.com>
2022-07-01 21:28:07 -0700

    I didn't sleep last night

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Random number generator
+ * tell if a number is positive, negative or zero
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+	int putchar(int);
+
+	n = 0;
+	while (n < 10)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
