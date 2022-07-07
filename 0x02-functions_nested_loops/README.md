57d4ec5db1e537875d55fa36087091f974b55a7a
Chizoba ODINAKA <chizobajames21@gmail.com>
2022-07-07 11:57:08 -0700

    Learning C function, and the nested loops

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+#0x02. C - Functions, nested loops
+
+/**
+*0-putchar.c - this is a program that uses _putchar to print
+*
+*/
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  main - calls my other functions
+ *
+ *
+ * return -zero(0)
+ */
+
+int main(void)
+{
+	print_putchar();
+	return (0);
+}
