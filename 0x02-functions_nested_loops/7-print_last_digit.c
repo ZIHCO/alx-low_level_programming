799b8ff6f89a4aa22a7cf466a015f5b93280797f
Chizoba ODINAKA <chizobajames21@gmail.com>
2022-07-08 08:57:24 -0700

    Learning C functions, and nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_last_digit - print the last digit of a number 
+ * print_last_digit - print the last digit of a number
  * @n: integers
  * Return: on success n % 10
  * Description: last digit finder
  */
 int print_last_digit(int n)
 {
-	int x;
-	
+	int x
+
 	if (n >= 0)
 	{
 		x = (n % 10) + 48;
