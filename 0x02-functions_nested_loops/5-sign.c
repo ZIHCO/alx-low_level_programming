e2e55895ef6298d83193df1761180446e4a0f600
Chizoba ODINAKA <chizobajames21@gmail.com>
2022-07-08 04:05:18 -0700

    Learning C functions, and nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of arg
+ * @n: n is a number
+ * Return: on success 1, 0 , -1
+ * Description: check if a number is +, 0, -
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);	
+	}
+}
