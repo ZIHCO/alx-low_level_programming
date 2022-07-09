bf0eb00f5e525ae248929365ad948dc373a9e632
Chizoba ODINAKA <chizobajames21@gmail.com>
2022-07-09 05:26:06 -0700

    Learning C functions, and nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - sum up two integers
+ * @num1: num1 is an integer
+ * @num2: num2 is an integer
+ * Return: on success return num1 + num2
+ * Description: calculate the sum of 2 integers passed as arguments
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
