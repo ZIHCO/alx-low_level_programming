b0c1857d150e0ae3987d6612ce47f22156a919af
Chizoba ODINAKA <chizobajames21@gmail.com>
2022-07-09 03:10:08 -0700

    Learning C functions, and nested loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Description: prints the multiplications upto the number 9
+ */
+void times_table(void)
+{
+	int num1, num2;
+	int product, tens, unit;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+
+			product = num1 * num2;
+			if (product > 9)
+			{
+				tens = product / 10;
+				unit = (product) % 10;
+				_putchar('0' + tens);
+				_putchar('0' + unit);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('0' + product);
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
