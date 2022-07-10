b1625805efba076cd6a60ae1962fb4d56161568e
Chizoba ODINAKA <chizobajames21@gmail.com>
2022-07-10 00:10:16 -0700

    Learning C functions, and nested loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,25 +8,17 @@
 int main(void)
 {
 	int sum3_5multiples, sum_multiples3, sum_multiples5, multiple3, multiple5;
-	int i, j;
 
-	multiple3 = 3;
-	multiple5 = 5;
 	sum_multiples3 = 0;
 	sum_multiples5 = 0;
-	i = 1;
-	j = 1;
-	while (multiple3 < 1024)
+	for (multiple3 = 3; multiple3 < 1024; multiple3 += 3)
 	{
-		multiple3 *= i;
 		sum_multiples3 += multiple3;
-		i++;
 	}
-	while (multiple5 < 1024)
+	for ( multiple5 = 5; multiple5 < 1024; multiple5 += 5)
 	{
-		multiple5 *= j;
-		sum_multiples5 += multiple5;
-		j++;
+		if (multiple5 % 3 != 0)
+			sum_multiples5 += multiple5;
 	}
 	sum3_5multiples = sum_multiples5 + sum_multiples3;
 	printf("%d\n", sum3_5multiples);
