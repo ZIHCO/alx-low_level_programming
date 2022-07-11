e42a81ee1d8aedcaca9ee3f786f938f6e89c1939
ZIHCO <chizobajames21@gmail.com>
2022-07-11 18:26:58 +0000

    Learning C function, and nested loops

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print the first 98 fibonacci
+ * Return: on success return 0
+ * Description: print fibonacci starting with 1, 2
+ */
+void main(void)
+{
+	int i;
+	float incremental, num;
+
+	i = 1;
+	num = 2;
+	incremental = 1;
+	while (i < 62)
+	{
+		if (i < 62)
+		{
+			printf("%.0f, ", incremental);
+			incremental += num;
+			printf("%.0f, ", num);
+			num += incremental;
+		}
+		i++;
+	}
+	printf("%.0f, ", incremental);
+	printf("%.0f\n", num);
+}
