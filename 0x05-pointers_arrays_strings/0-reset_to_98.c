14a6b7e1f757e47b15cd0a0bcd4d27c682c7a2b9
ZIHCO <chizobajames21@gmail.com>
2022-07-13 03:19:33 +0000

    Learning about C pointers, arrays, and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - update the value of an integer via the pointer
- * @*n: is apointer to an integer
+ * @n: is apointer to an integer
  * Description: takes a pointer to an integer as parameter, and update
  * the value it points to to 98
  */
@@ -10,5 +10,4 @@
 void reset_to_98(int *n)
 {
 	*n = 98;
-	return;
 }
