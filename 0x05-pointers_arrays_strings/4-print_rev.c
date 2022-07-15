ab165507a2aadfd643e727545fe2b7ced90987ce
ZIHCO <chizobajames21@gmail.com>
2022-07-15 22:17:00 +0000

    Learning about C pointers, arrays, and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse order
+ * @s: take a string as argument
+ * Description: take a string as argument and print the reversal of the string
+ */
+void print_rev(char *s)
+{
+	int i, j, k;
+
+	k = 0;
+	for (j = 0; s[j] != '\0'; j++)
+		k += 1;
+	_putchar(k);
+	for (i = k - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
