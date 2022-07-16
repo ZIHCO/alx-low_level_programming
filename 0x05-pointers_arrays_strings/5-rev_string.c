9e21f54c665e53cb9762bb7b153e3757ae2c64e3
ZIHCO <chizobajames21@gmail.com>
2022-07-16 11:04:24 +0000

    Learning about C pointers, arrays, and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,13 +8,16 @@
 void rev_string(char *s)
 {
 	int i, j, k;
+	char tmp;
 
 	k = 0;
 	for (j = 0; s[j] != '\0'; j++)
 		k += 1;
-	for (i = k - 1; i >= 0; i--)
+	for (i = k - 1; i >= k / 2; i--)
 	{
-		_putchar(s[i]);
+		tmp = s[i];
+		s[i] = s[k - i - 1];
+		s[k - i - 1] = tmp;
 	}
 	_putchar('\n');
 }
