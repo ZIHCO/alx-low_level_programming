83e776e4806f8046074f3b36d18798c000e0186c
ZIHCO <chizobajames21@gmail.com>
2022-07-16 13:41:15 +0000

    Learning C pointers, arrays, and strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half -  print the second half of the string
+ * @str: take a string as an argument
+ * Description: The function should print the second half of the string.
+ * If the number of characters is odd, the function should print the last
+ * n characters of the string, where n = (length_of_the_string - 1) / 2
+ */
+void puts_half(char *str)
+{
+	int i, j, k;
+
+	j = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		j++;
+	for (k = j / 2; k <= j - 1; k++)
+	{
+		_putchar(str[k]);
+	}
+	_putchar('\n');
+}
