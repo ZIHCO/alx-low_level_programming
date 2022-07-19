49a202f7113ef36f7f91f23aaf692a64ea68d022
ZIHCO <chizobajames21@gmail.com>
2022-07-19 05:13:43 +0000

    Learning pointers to pointers, and dereferencing dereference to dereferences

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: string constant to search
+ * @c: character to search for
+ * Return: the pointer to the c in s, or NULL
+ * Description: The standard library provides a similar
+ * function: strchr. Run man strchr to learn more.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *pcs;
+
+	for (i = 0; s[i] != c; i++)
+		;
+	if (s[i] == c)
+		pcs =&s[i];
+	else
+		return ('\0');
+	return (pcs);
+}
+
