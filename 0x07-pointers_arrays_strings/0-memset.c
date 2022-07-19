d1ab2a23f1cca3979327a006dc95f2f967468b4b
ZIHCO <chizobajames21@gmail.com>
2022-07-19 04:24:26 +0000

    Learning pointers to pointers, and dereferencing dereference to dereferences

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset -  fills memory with a constant byte.
+ * @s: pointer to a memory area
+ * @b: a constant to fill s with
+ * @n: the number of times to do the filling
+ * Return: on success a pointer to s
+ * Description: : The standard library provides a similar
+ * function: memset. Run man memset to learn more.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
