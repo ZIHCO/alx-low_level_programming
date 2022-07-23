ac8804f3f05105307aa71a89c6677762d0c7add3
ZIHCO <chizobajames21@gmail.com>
2022-07-23 11:26:33 +0000

    Learning argv, and argc

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 		{
 			for (j = 0; j < strlen(argv[i]); j++)
 			{
-				if (!(argv[i][j] < 57 && argv[i][j] > 47))
+				if (argv[i][j] > 57 || argv[i][j] < 48)
 				{
 					printf("Error\n");
 					return (1);
