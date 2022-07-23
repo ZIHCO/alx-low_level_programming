8804dc7635ad519366513c26da6764f0bc8c7f65
ZIHCO <chizobajames21@gmail.com>
2022-07-23 06:54:05 +0000

    Learning argv, and argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: number of command line arguments
+ * @argv: single argument vector
+ * Return: 0 on success
+ * Description: print a number, followed by a new line
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
