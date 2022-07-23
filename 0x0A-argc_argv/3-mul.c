391d68bfbfaa69ee76db27bf6eb55587baf655e5
ZIHCO <chizobajames21@gmail.com>
2022-07-23 08:09:44 +0000

    Learning argv, and argc

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all arguments it receives.
+ * @argc: number of command line arguments
+ * @argv: single argument vector
+ * Return: 0 on success
+ * Description: All arguments should be printed, including
+ * the first one.
+ * Only print one argument per line, ending with a new line.
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+	int y;
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	if (argc < 3)
+	{
+		printf("%s\n", "error");
+		return (1);
+	}
+	printf("%d\n", x * y);
+	return (0);
+}
