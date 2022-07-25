8a359fcc4b6f3aff95d9a1f87944939e9a50d889
ZIHCO <chizobajames21@gmail.com>
2022-07-25 14:29:26 +0000

    More practise on functions, and nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer argument
+ * Description: print integers using _putchar
+ */
+void print_number(int n)
+{
+	int i, power, digit, num;
+
+	i = 1;
+	power = 1;
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	num = n;
+	while (num / 10 > 0)
+	{
+		num = num / 10;
+		i++;
+	}
+	while (i > 1)
+	{
+		power *= 10;
+		i--;
+	}
+	while (power >= 1)
+	{
+		digit = n / power;
+		_putchar(48 + digit);
+		n = n - (power * digit);
+		power = power / 10;
+	}
+}
