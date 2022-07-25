a847e6f837ebb29d9df67d51b5a4bfafe1d063e6
ZIHCO <chizobajames21@gmail.com>
2022-07-25 18:17:45 +0000

    Learning argv, and argc

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,9 +16,8 @@
  */
 int main(int argc, char *argv[])
 {
-	long int bal, coin, p;
+	long int coin, p;
 
-	bal = 0;
 	coin = 0;
 	if (argc != 2)
 	{
@@ -27,72 +25,34 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	p = atoi(argv[1]);
-	if (p >= 25)
+	while (p != 0)
 	{
-		if (p % 25 == 0)
+		while (p >= 25)
 		{
-			coin = p / 25;
+			p = p - 25;
+			coin++;
 		}
-		else
+		while (p >= 10)
 		{
-			coin = p / 25;
-			bal = p % 25;
+			p = p - 10;
+			coin++;
+		}
+		while (p >= 5)
+		{
+			p = p - 5;
+			coin++;
+		}
+		while (p >= 2)
+		{
+			p = p - 2;
+			coin++;
+		}
+		if (p == 1)
+		{
+			coin += 1;
+			break;
 		}
 	}
-	p = p % 25;
-	if (p >= 10)
-	{
-		if (p % 10 == 0 && bal == 0)
-		{
-			coin = p / 10;
-		}
-		else if (bal % 10 == 0 && bal != 0)
-		{
-			coin += bal / 10;
-		}
-		else
-		{
-			coin += p / 10;
-			bal = p % 10;
-		}
-	}
-	p = p % 10;
-	if (p >= 5)
-	{
-		if (p % 5 == 0 && bal == 0)
-		{
-			coin = p / 5;
-		}
-		else if (bal % 5 == 0 && bal != 0)
-		{
-			coin += bal / 5;
-		}
-		else
-		{
-			coin += p / 5;
-			bal = p % 5;
-		}
-	}
-	p = p % 5;
-	if (p >= 2)
-	{
-		if (p % 2 == 0 && bal == 0)
-		{
-			coin = p / 2;
-		}
-		else if (bal % 2 == 0 && bal != 0)
-		{
-			coin += bal / 2;
-		}
-		else
-		{
-			coin += p / 2;
-			bal = p % 2;
-		}
-	}
-	p = p % 2;
-	if (p >= 1)
-		coin += 1;
 	printf("%ld\n", coin);
 	return (0);
 }
