9f7ee7fd365a0d1ed10db9913503d9e294e0c83d
ZIHCO <chizobajames21@gmail.com>
2022-07-26 15:35:50 +0000

    Learning about malloc, and free(dalloc)

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars
+ * @size: the size of memory to alocate
+ * @c: the char to assign to memory
+ * Return: NULL or a pointer to the array
+ * Description: Returns NULL if size = 0
+ * Returns a pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *pc;
+	unsigned int i;
+
+	pc = malloc(sizeof(*pc) * size);
+	i = 0;
+	if (size == 0)
+		return ('\0');
+	while (i < size)
+	{
+		pc[i] = c;
+		i++;
+	}
+	return (pc);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+#0x0B-malloc_free
+
+Learning memory allocation (malloc), and deallocation (free).
