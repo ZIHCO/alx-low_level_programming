2cf6dca863f45cf5a760baa8caf02d61e7c6e4ce
ZIHCO <chizobajames21@gmail.com>
2022-07-27 00:00:53 +0000

    Learning about malloc, and free (dalloc)

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * str_concat - that concatenates two strings.
@@ -17,26 +18,38 @@ char *str_concat(char *s1, char *s2)
 	char *ps;
 	unsigned long i, j, l, k;
 
-	i = 0;
-	j = 0;
 	k = 0;
 	l = 0;
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
+	i = strlen(s1);
+	j = strlen(s2);
 	ps = malloc(sizeof(char) * (i + j) + 1);
 	if (ps == NULL)
 		return (NULL);
-	while (k < i)
+	if (s1 == NULL)
+	{
+		while (l <= j)
+		{
+			ps[l] = s2[l];
+			l++;
+		}
+	}
+	if (s2 == NULL)
+	{
+		while (k <= i)
+		{
+			ps[k] = s1[k];
+			k++;
+		}
+	}
+	while (k < i && s1 != NULL)
 	{
 		ps[k] = s1[k];
 		k++;
 	}
 	ps[i] = ' ';
-	while (l <= j)
+	while (l <= j && s2 != NULL)
 	{
 		ps[l + i] = s2[l];
 		l++;
