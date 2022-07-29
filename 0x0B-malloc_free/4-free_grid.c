b225372d9d3f74893f2ae0ce1d62d7bd2eafea34
ZIHCO <chizobajames21@gmail.com>
2022-07-29 07:34:32 +0000

    Learning about malloc, and free(dalloc)

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid
+ * @grid: the grid to free
+ * @height: the number of rows
+ * Description: Note that we will compile with your
+ * alloc_grid.c
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = height; i >= 0; i--)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
