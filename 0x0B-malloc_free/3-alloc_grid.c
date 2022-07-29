040525f4f588932ac1ef8ab9ef02f202ff6ed238
ZIHCO <chizobajames21@gmail.com>
2022-07-29 05:04:48 +0000

    Learning about malloc, and free(dalloc)

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional
+ * array of integers.
+ * @width: number of columns
+ * @height: number of rows
+ * Return: NULL on failure, or a pointer
+ * Description: Each element of the grid should be
+ * initialized to 0.
+ * If width or height is 0 or negative, return NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **pa;    
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	pa = malloc(sizeof(int *) * height);
+	if (pa == NULL)
+	{
+		free(pa);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		pa[i] = malloc(sizeof(int) * width);
+		if (pa[i] == NULL)
+		{
+			for (j = i; 0 <= j; j--)
+			{
+				free(pa[j]);
+				return (NULL);
+			}
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			pa[i][j] = 0;
+		}
+	}
+	return (pa);
+}
