00035a38878ca02a1038eea5a5486bc1515ee1cd
ZIHCO <chizobajames21@gmail.com>
2022-08-05 09:53:45 +0000

    Project structures, and typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -21,7 +21,7 @@ void print_dog(struct dog *d)
 		if (d->age)
 			printf("Age: %f\n", d->age);
 		else
-			printf("Age: 0");
+			printf("Age: %f\n", 0.0);
 		if (d->owner == NULL)
 			printf("(nil)\n");
 		else
