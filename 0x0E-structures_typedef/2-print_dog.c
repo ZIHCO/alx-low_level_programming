3f4688e3d8519d9556d0ee918040b50e3fc8fbda
ZIHCO <chizobajames21@gmail.com>
2022-08-05 08:56:48 +0000

    Project structures, and typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - prints a struct dog
+ * @d: a pointer to a struct dog
+ *
+ * Description: If an element of d is NULL, print (nil) instead
+ * of this element. (if name is NULL, print Name: (nil))
+ * If d is NULL print nothing.
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		exit(0);
+	if (d->name == NULL)
+		printf("(nil)");
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age == 0)
+		printf("(nil)");
+	else
+		printf("Age: %f\n", d->age);
+	if (d->owner ==NULL)
+		printf("(nil)");
+	else
+		printf("Owner: %s\n", d->owner);
+}
