efc3c597c44ab77b71856885a0ca234d4f665660
ZIHCO <chizobajames21@gmail.com>
2022-08-05 10:05:29 +0000

    Project structures, and typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,12 +15,12 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name == NULL)
-			printf("(nil)\n");
+			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
 		printf("Age: %f\n", (d->age) ? d->age : 0);
 		if (d->owner == NULL)
-			printf("(nil)\n");
+			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", d->owner);
 	}
