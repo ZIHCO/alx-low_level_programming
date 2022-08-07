a534710690b0c19eb2a42e2a88f537280aa00b94
ZIHCO <chizobajames21@gmail.com>
2022-08-07 13:04:56 +0000

    Project on pointers to functions

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,13 +15,13 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 	ps = get_op_func(argv[2]);
 	if (!ps)
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
 	printf("%d\n", ps(atoi(argv[1]), atoi(argv[3])));
 	return (0);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -51,7 +51,7 @@ int op_div(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	return (a / b);
 }
@@ -70,7 +70,7 @@ int op_mod(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	return (a % b);
 }
