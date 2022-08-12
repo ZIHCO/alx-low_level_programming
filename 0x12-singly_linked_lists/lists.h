a0b9a59a9b35c6db4d5a3a0b07551c10176409ed
ZIHCO <chizobajames21@gmail.com>
2022-08-12 10:29:19 +0000

    Get the length of a linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef _HEADER_
 #define _HEADER_
 
+typedef __SIZE_TYPE__ size_t;
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -17,7 +18,7 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
-int _putchar(char c);
 size_t list_len(const list_t *h);
+int _putchar(char c);
 
 #endif
