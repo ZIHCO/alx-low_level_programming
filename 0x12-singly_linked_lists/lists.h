33fc5210cc79aa3de4de9f5f1ec33f323c9c9d76
ZIHCO <chizobajames21@gmail.com>
2022-08-15 17:44:35 +0000

    Free this memory

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -23,5 +23,6 @@ int _putchar(char c);
 list_t *add_node(list_t **head, const char *str);
 int _strlen(const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
