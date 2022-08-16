2af0967c99cd6a88c9f8d86a6e57b601f85f0ba2
ZIHCO <chizobajames21@gmail.com>
2022-08-16 22:11:13 +0000

    Delete this node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,11 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		nextnode = node->next;
 		while (n < (index - 1))
 		{
-			if (node == NULL)
-			{
-				return (-1);
-			}
-			else if (nextnode == NULL && n == (index - 2))
+			if (nextnode == NULL && n == (index - 2))
 			{
 				free(node);
 				return (1);
