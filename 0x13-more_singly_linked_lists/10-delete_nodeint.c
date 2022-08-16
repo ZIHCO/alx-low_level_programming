3eccf4671657184d095cd987dcdced82ea43a8bf
ZIHCO <chizobajames21@gmail.com>
2022-08-16 20:35:49 +0000

    Delete this node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -33,7 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			{
 				return (-1);
 			}
-			else if (nextnode == NULL)
+			else if (nextnode == NULL && n == (index -2))
 			{
 				free(node);
 				return (1);
