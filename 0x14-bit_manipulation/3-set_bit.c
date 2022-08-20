a76c7041d7c80f993fb53f04a00e01b56839d415
ZIHCO <chizobajames21@gmail.com>
2022-08-20 12:54:05 +0000

    Set bit to 1 at index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index.
+ * @n: pointer to an integer variable
+ * @index: bit index
+ * Return: 1 0n success or -1 on failure
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int m = *n;
+	unsigned long int sum = 0;
+	unsigned int i = 0;
+	int b = 0;
+
+	if (m == 0 && index > 0)
+	{
+		while (i < 31)
+		{
+			if (i == index)
+				sum += 1 << i;
+			i++;
+		}
+		*n = sum;
+		return (1);
+	}
+	if (m > 0 && index == 0)
+	{
+		*n = m + 1;
+		return (1);
+	}
+	if (m > 0 && index > 0)
+	{
+		while (i < 31)
+		{
+			if (m && i != index)
+			{
+				m = m >> 1;
+				b = m & 1;
+				sum += b << i;
+			}
+			if (i == index)
+				sum += 1 << index;
+			i++;
+		}
+		*n = sum;
+		return (1);
+	}
+	return (-1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,6 @@ int _strlen(const char *b);
 void print_binary(unsigned long int n);
 void rightshift(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
