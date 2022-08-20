da9c1ab330f756aca3f2955c1f04a9333c78b5f5
ZIHCO <chizobajames21@gmail.com>
2022-08-20 09:05:37 +0000

    Get this index bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,14 +10,24 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int b;
+	unsigned int i = 0;
 
 	b = 0;
-	if (n <= 0)
-		return (-1);
-	while (index--)
+	if (index == 0)
 	{
 		n = n >> 1;
 		b = n & 1;
+		return (b);
 	}
-	return (b);
+	while (n)
+	{
+		n = n >> 1;
+		b = n & 1;
+		i++;
+		if (index == i)
+			return (b);
+	}
+	if (index > i && index < 63)
+		return (0);
+	return (-1);
 }
