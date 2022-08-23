93bf8e083193f9aed125a352d90661e3cd71d750
ZIHCO <chizobajames21@gmail.com>
2022-08-23 04:38:08 +0000

    File I/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,7 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd = open(filename, O_RDONLY, 0700);
+	int fd = open(filename, O_RDONLY, 0600);
 	char *buff = malloc(sizeof(char *) * letters);
 	ssize_t actual_count;
 
