2e937605a5cc4e4518be4df315b582a14df944e6
ZIHCO <chizobajames21@gmail.com>
2022-09-17 06:15:00 +0000

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,9 +17,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		mem = malloc((nmemb + 1) * size);
 		if (mem == NULL)
 			return (NULL);
-		for (i = 0; i < nmemb; i++)
+		for (i = 0; i < nmemb * size; i++)
 			mem[i] = 0;
-		mem[nmemb] = '\0';
+		mem[nmemb * size] = '\0';
 	}
 	return (mem);
 }
