a0e291c867dd911a483eed88470dae26342de177
ZIHCO <chizobajames21@gmail.com>
2022-09-23 20:30:00 +0000

    I just learnt

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -11,7 +11,7 @@ void mystartup(void)
 	char *str;
 	char *str1;
 
-	str = "You're beat! and yet, you must allow, \n";
+	str = "You're beat! and yet, you must allow,\n";
 	str1 = "I bore my house upon my back!\n";
 	printf("%s%s", str, str1);
 }
