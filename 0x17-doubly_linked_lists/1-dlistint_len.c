53b23a1f47286940409ad63a6a188ed0befb7041
ZIHCO <chizobajames21@gmail.com>
2022-09-30 02:21:44 +0000

    Number of nodes

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked list.
+ * @h: head of the linked
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
