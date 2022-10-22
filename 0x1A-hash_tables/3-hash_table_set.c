a20d6c5ca0cb35aa70496281c85670e24b5d0a2b
ZIHCO <chizobajames21@gmail.com>
2022-10-22 21:20:45 +0000

    Add to this index

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: hash table
+ * @key: key to be add
+ * @value: the value to add
+ * Return: 1 if successful, 0 otherwisw
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node;
+	unsigned long int hashcode, size, idx;
+
+	if (ht && strlen(key) > 0)
+	{
+		node = malloc(sizeof(hash_node_t));
+		if (node == NULL)
+			return (0);
+		node->key = strdup(key);
+		node->value = strdup(value);
+		node->next = NULL;
+
+		hashcode = hash_djb2((unsigned char *) key);
+		size = ht->size;
+		idx = hashcode % size;
+
+		if (ht->array[idx] == NULL)
+		{
+			ht->array[idx] = node;
+			return (1);
+		}
+
+		node->next = ht->array[idx];
+		ht->array[idx] = node;
+		return (1);
+	}
+	return (0);
+}
