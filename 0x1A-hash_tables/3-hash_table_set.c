c6ef479a7e0f248fca7bb843ffb2caae95d4b16c
ZIHCO <chizobajames21@gmail.com>
2022-10-23 04:56:37 +0000

    insert to a hashmap

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node;
 	unsigned long int size, idx;
 
-	if (ht && strlen(key) > 0)
+	if (ht && strlen(key) > 0 && strlen(value) > 0)
 	{
 		node = malloc(sizeof(hash_node_t));
 		if (node == NULL)
