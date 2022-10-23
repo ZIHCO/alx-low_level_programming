c979db5f3f0fcffe2c325c16555c09a30a942007
ZIHCO <chizobajames21@gmail.com>
2022-10-23 15:32:21 +0000

    print the hashtable

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -22,7 +22,7 @@ void hash_table_print(const hash_table_t *ht)
 			{
 				if (flag == 1)
 					printf(", ");
-				printf("'%s' : '%s'", node->key, node->value);
+				printf("'%s': '%s'", node->key, node->value);
 				node = node->next;
 				flag = 1;
 			}
