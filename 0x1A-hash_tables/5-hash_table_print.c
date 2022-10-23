b9f989208d12722ad897b90ee83a5cd34d8cd8d8
ZIHCO <chizobajames21@gmail.com>
2022-10-23 15:30:52 +0000

    print the hashtable

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - that prints a hash table
+ * @ht: is the hash table
+ * Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i, size, flag;
+
+	if (ht && ht->array)
+	{
+		flag = 0;
+		size = ht->size;
+		printf("{");
+		for (i = 0; i < size; i++)
+		{
+			node = ht->array[i];
+			while (node)
+			{
+				if (flag == 1)
+					printf(", ");
+				printf("'%s' : '%s'", node->key, node->value);
+				node = node->next;
+				flag = 1;
+			}
+		}
+		printf("}\n");
+	}
+}
