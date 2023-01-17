76f1bc5955f3b43b5cf1df65740ecaf5658b4217
ZIHCO <chizobajames21@gmail.com>
2023-01-17 08:41:35 +0000

    going back is beautiful

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  * Description: a dog's attributes
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
