e9e10772f1f391b466c634eaa4809c7308747921
ZIHCO <chizobajames21@gmail.com>
2024-01-22 04:41:39 +0100

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,13 @@
 /**
  * main - write with the unix write()
  * args - void
- * return - 1
+ * Return: 1
  */
 
-int main()
+int main(void)
 {
-	char * str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, str, strlen(str));
 	return (1);
 }
