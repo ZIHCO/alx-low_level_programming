4ac179813a7c7594dec4e49c4146d5c68ce2fdc1
zihco <chizobajames21@gmail.com>
2024-02-25 11:50:55 +0100

    impliment rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,26 +9,18 @@
 char *rot13(char *s)
 {
 	int i, j;
-	char letter[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-		       'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-		       's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
-		       'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-		       'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-		       'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '\0'};
-	char letter13[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-			 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e',
-			 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'N',
-			 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-			 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F',
-			 'G', 'H', 'I', 'J', 'K', 'L', 'M', '\0'};
-
+	char letter[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char letter13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; letter[j] != '\0'; j++)
 		{
 			if (letter[j] == s[i])
+			{
 				s[i] = letter13[j];
+				break;
+			}
 		}
 	}
 	return (s);
