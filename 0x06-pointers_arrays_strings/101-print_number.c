1ce324a31da7c6f1bd21ccc7d20caae29f0ff7b5
zihco <chizobajames21@gmail.com>
2024-02-28 10:23:04 +0100

    unsigned int

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,29 +8,25 @@
 
 void print_number(int n)
 {
-	int power10 = 10;
-	int new;
+	unsigned int power10 = 1;
+	unsigned int new;
 
 	if (n < 0)
 	{
-		n *= -1;
+		new = -1 * n;
 		_putchar('-');
-	}
-	if (n >= 0 && n < 10)
-	{
-		_putchar(48 + n);
 	}
 	else
 	{
-		while (power10 <= n / 10)
-			power10 *= 10;
-
 		new = n;
-		while (power10 > 0)
-		{
-			_putchar((new / power10) + 48);
-			new = new % power10;
-			power10 /= 10;
-		}
+	}
+	while (power10 <= new / 10)
+		power10 *= 10;
+
+	while (power10 > 0)
+	{
+		_putchar((new / power10) + 48);
+		new = new % power10;
+		power10 /= 10;
 	}
 }
