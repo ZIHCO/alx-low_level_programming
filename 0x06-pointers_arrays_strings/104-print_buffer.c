9a3dd95101483607cfef0df3b5e1dd7f94885882
zihco <chizobajames21@gmail.com>
2024-03-01 19:28:42 +0100

    memory control

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -17,7 +17,10 @@ void print_buffer(char *b, int size)
 		printf("%08x: ", i);
 		for (count10 = 0, count2 = 0; count10 < 10; count10++)
 		{
-			printf("%.2x", *(b + i + count10));
+			if (i + count10 > size - 1)
+				printf("%2c", ' ');
+			else
+				printf("%.2x", *(b + i + count10));
 			if (count2 == 1)
 			{
 				printf("%c", ' ');
@@ -30,22 +33,28 @@ void print_buffer(char *b, int size)
 		}
 		for (count2 = 0, count10 = 0; count10 < 10; count10++)
 		{
-			if (*(b + count10 + i) != '\0' &&
-			    *(b + i + count10) != '\1' &&
-			    *(b + i + count10) != '\2' &&
-			    *(b + i + count10) != '\3' &&
-			    *(b + i + count10) != '\4' &&
-			    *(b + i + count10) != '\5' &&
-			    *(b + i + count10) != '\6' &&
-			    *(b + i + count10) != '\7' &&
-			    *(b + i + count10) != '\n')
-				printf("%c", *(b + i + count10));
+			if (i + count10 > size - 1)
+			{
+				printf("%2c", ' ');
+			}
 			else
-				printf("%c", '.');
-			if (count10 == 9)
-				printf("\n");
+			{
+				if (*(b + count10 + i) != '\0' &&
+				    *(b + i + count10) != '\1' &&
+				    *(b + i + count10) != '\2' &&
+				    *(b + i + count10) != '\3' &&
+				    *(b + i + count10) != '\4' &&
+				    *(b + i + count10) != '\5' &&
+				    *(b + i + count10) != '\6' &&
+				    *(b + i + count10) != '\7' &&
+				    *(b + i + count10) != '\n')
+					printf("%c", *(b + i + count10));
+				else
+					printf("%c", '.');
+				if (count10 == 9)
+					printf("\n");
+			}
 		}
 	}
-	if (size <= 0)
-		printf("\n");
+	printf("\n");
 }
