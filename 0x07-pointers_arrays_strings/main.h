c79736ed198886101d36e9dcd87a559b7be28307
zihco <chizobajames21@gmail.com>
2024-03-03 17:40:15 +0100

    done

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int length_needle(char *str);
 void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 
 #endif
